5fcd21ff10d5f709100aa9ef5daeb96b5f1ab35c
Asmit Panigrahi <128639526+AsmitPanigrahi@users.noreply.github.com>
2023-12-29 13:24:08 +0530

    Time: 23 ms (49.14%) | Memory: 12.4 MB (10.52%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,14 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> output;
+        unordered_map<int, int> freq;
+        int n = nums.size();
 
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
+        for (int i = 0; i < n; i++) {
+            freq[nums[i]]++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+
+            if (freq.count(complement) > 0) {
+                if (complement == nums[i] && freq[complement] > 1) {
+                    // If the complement is the same as the current number
                     output.push_back(i);
-                    output.push_back(j);
-                    return output;
+                    for (int j = i + 1; j < n; j++) {
+                        if (nums[j] == complement) {
+                            output.push_back(j);
+                            return output;
+                        }
+                    }
+                } else if (complement != nums[i]) {
+                    // If the complement is different
+                    output.push_back(i);
+                    for (int j = 0; j < n; j++) {
+                        if (nums[j] == complement) {
+                            output.push_back(j);
+                            return output;
+                        }
+                    }
                 }
             }
         }
